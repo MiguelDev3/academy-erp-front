f31925b76c6dc1570095a68a1b6ce4ec42f90cc6
MiguelDev3 <miguel.edu.chacon.3@gmail.com>
2025-08-15 15:56:35 -0500

    FEAT: Add Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,16 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Login } from "./pages/login/Login";
+import { Register } from "./pages/register/Register";
 
 function App() {
   return (
-    <>
-    <main className="bg-[#3c82f6] min-h-dvh flex justify-center items-center">
-      <div className="relative py-3 sm:max-w-xs sm:mx-auto">
-        <div className="min-h-96 px-8 py-6 mt-4 text-left bg-white dark:bg-gray-900 rounded-xl shadow-lg">
-          <div className="flex flex-col justify-center items-center h-full select-none">
-            <div className="flex flex-col items-center justify-center gap-2 mb-8">
-              <img className="w-24 h-16" src="/logo-enriques 1.png"></img>
-              <p className="m-0 text-[16px] font-semibold dark:text-white">
-                Ingresa a tu cuenta
-              </p>
-              <span className="m-0 text-xs max-w-[90%] text-center text-[#8B8E98]">
-                Sistema para usuarios de la academia
-              </span>
-            </div>
-            <div className="w-full flex flex-col gap-2">
-              <label className="font-semibold text-xs text-gray-400">
-                Username
-              </label>
-              <input
-                placeholder="Username"
-                className="border rounded-lg px-3 py-2 mb-5 text-gray-500 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"
-              />
-            </div>
-          </div>
-          <div className="w-full flex flex-col gap-2">
-            <label className="font-semibold text-xs text-gray-400">Password</label>
-            <input
-              placeholder="••••••••"
-              className="border rounded-lg px-3 py-2 mb-5 text-gray-500 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"
-              type="password"
-            />
-          </div>
-          <div>
-            <button className="py-1 px-8 bg-blue-500 hover:bg-blue-800 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none">
-              Ingresar
-            </button>
-          </div>
-        </div>
-      </div>
-    </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
