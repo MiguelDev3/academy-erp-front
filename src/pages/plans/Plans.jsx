3e46c96621f4e1a4936d5681c30a4c55742741b9
MiguelDev3 <miguel.edu.chacon.3@gmail.com>
2025-09-01 18:42:46 -0500

    Update Plans Page

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,7 +1,54 @@
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  createColumnHelper,
+} from "@tanstack/react-table";
 import { HamburgerButton } from "../../components/ui/HamburgerButton";
 import { Sidebar } from "../../components/ui/sidebar";
+import { useState } from "react";
 
+const defaultData = [
+  {
+    _id: 1,
+    name: "Ligero",
+    hoursPerWeeK: 2,
+    cost: 150.0,
+  },
+];
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor("_id", {
+    header: (info) => "ID",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("name", {
+    header: () => "Nombre",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("hoursPerWeeK", {
+    header: () => "Horas/semana",
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id
+  }),
+  columnHelper.accessor("cost", {
+    header: () => "Costo",
+    cell: (info) => `S/ ${info.getValue()}`,
+    footer: (info) => info.column.id
+  })
+];
 export const Plans = ({ menuClosed, toggleMenu }) => {
+  const [data, _setData] = useState([...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   return (
     <>
       <Sidebar menuClosed={menuClosed} toggleMenu={toggleMenu} />
@@ -11,6 +58,59 @@ export const Plans = ({ menuClosed, toggleMenu }) => {
         <section className="[--shadow:rgba(60,64,67,0.3)_0_1px_2px_0,rgba(60,64,67,0.15)_0_2px_6px_2px] w-full h-auto rounded-2xl bg-white [box-shadow:var(--shadow)]">
           <div className="w-full border-b border-gray-300">
             <h2 className="py-3 ps-3 font-medium text-sm">Vista general</h2>
+          </div>
+          <div className="w-full p-3">
+            <table>
+              <thead>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <tr key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <th key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody>
+                {table.getRowModel().rows.map((row) => (
+                  <tr key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                {table.getFooterGroups().map((footerGroup) => (
+                  <tr key={footerGroup.id}>
+                    {footerGroup.headers.map((header) => (
+                      <th key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.footer,
+                              header.getContext()
+                            )}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </tfoot>
+            </table>
+          </div>
+          <div className="w-full border-t border-gray-300 p-1 flex justify-end">
+            <button className="p-2 bg-green-700 text-white">+ Agregar plan</button>
           </div>
         </section>
       </main>
